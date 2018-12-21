2563678ffd8af85d8736c5d88df9399f366df2cb
paveltech <pavelcep.sust11@gmail.com>
2018-12-21 14:01:02 +0600

    locagic change

--- app/src/main/java/com/pavel/responsecode/MainActivity.java
+++ app/src/main/java/com/pavel/responsecode/MainActivity.java
@@ -32,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 if (!httpResponseCode.checkResponse(200)){
-                    // do what ever you want
+                    /**
+                     *
+                     * do what ever you want if status code is not 200 then automaticaly status will show through a toast
+                     *
+                     */
                 }
             }
         });
